f36adc30a8ad8197617e45aab44180dacf5aa467
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-09-20 11:18:49 -0500

    commit - MotoService .

--- src/main/java/com/ejemplo/tiendamotos/service/MotoService.java
+++ src/main/java/com/ejemplo/tiendamotos/service/MotoService.java
@@ -1,4 +1,29 @@
 package com.ejemplo.tiendamotos.service;
 
+import com.ejemplo.tiendamotos.model.Moto;
+import com.ejemplo.tiendamotos.repository.MotoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MotoService {
+
+    private final MotoRepository motoRepository;
+
+    public MotoService(MotoRepository motoRepository) {
+        this.motoRepository = motoRepository;
+    }
+
+    public List<Moto> listarTodas() {
+
+        return motoRepository.findAll();
+    }
+
+    public Optional<Moto> buscarPorId(Long id)
+    {
+
+        return motoRepository.findById(id);
+    }   
 }
