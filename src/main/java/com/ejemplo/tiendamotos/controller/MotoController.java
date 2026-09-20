78d63651ad0ac51c8a1ac927b243146d7ad5147b
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-09-20 11:29:24 -0500

    commit - MotoController .

--- src/main/java/com/ejemplo/tiendamotos/controller/MotoController.java
+++ src/main/java/com/ejemplo/tiendamotos/controller/MotoController.java
@@ -1,4 +1,40 @@
 package com.ejemplo.tiendamotos.controller;
 
+import com.ejemplo.tiendamotos.model.Moto;
+import com.ejemplo.tiendamotos.service.MotoService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/motos")
 public class MotoController {
+
+    private final MotoService motoService;
+
+    public MotoController(MotoService motoService) {
+        this.motoService = motoService;
+    }
+
+    @GetMapping
+    public String listarMotos(
+            @RequestParam(required = false) String marca, Model model)
+    {
+        if (marca != null &&  !marca.isBlank())
+        {
+            model.addAttribute(
+                    "motos",
+                    motoService.buscarPorMarca(marca)
+            );
+        } else {
+            model.addAttribute(
+                    "motos",
+                    motoService.listarTodas()
+            );
+        }
+
+        model.addAttribute("marcaBusqueda", marca);
+
+        return "motos";
+    }   
 }
--- src/main/java/com/ejemplo/tiendamotos/service/MotoService.java
+++ src/main/java/com/ejemplo/tiendamotos/service/MotoService.java
@@ -40,5 +40,5 @@ public class MotoService {
     public void eliminar(Long id)
     {
         motoRepository.deleteById(id);
-    }   
+    }
 }
